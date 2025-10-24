5f8f4d68c8c6267002df0c30649f85b6dcc3c271
Pierre Pontet <ppontet@student.42lyon.fr>
2025-10-24 10:30:07 +0200

    feat: adding default config file if no argv[1]

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,31 @@
 
 #include "Debug.hpp"
 
+#define DEFAULT_CONFIG_FILE "conf/default.conf"
+
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+	if (argc > 2)
 	{
 		Debug::print(ERROR, "Usage: " + std::string(argv[0]) + " <config_file>");
 		return 1;
 	}
 	std::fstream configFile;
-
-	configFile.open(argv[1]);
+	std::string configFileName;
+	if (argc == 1)
+	{
+		Debug::print(INFO, "Using default config file : " + std::string(DEFAULT_CONFIG_FILE));
+		configFileName = DEFAULT_CONFIG_FILE;
+	}
+	else
+		configFileName = argv[1];
+	configFile.open(configFileName.c_str());
 	if (!configFile || !configFile.is_open())
 	{
-		Debug::print(ERROR, "Error opening config file: " + std::string(argv[1]));
+		Debug::print(ERROR, "Error opening config file: " + configFileName);
 		return 1;
 	}
-	Debug::print(INFO, "Config file " + std::string(argv[1]) + " opened successfully.");
+	Debug::print(INFO, "Config file " + configFileName + " opened successfully.");
 	configFile.close();
 	return 0;
 }
